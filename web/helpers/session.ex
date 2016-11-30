99b70503c1800f8ca504311e6d9edd6203c7ed5d
Claus Wahlers <claus@codeazur.com.br>
2016-11-30 05:52:19 -0200

    check for nil password/email on signin to prevent a 500

--- web/helpers/session.ex
+++ web/helpers/session.ex
@@ -1,9 +1,12 @@
 defmodule Piupiu.Session do
   alias Piupiu.{Repo, User}
 
+  def authenticate(%{"email" => nil, "password" => _password}), do: :error
+
+  def authenticate(%{"email" => _email, "password" => nil}), do: :error
+
   def authenticate(%{"email" => email, "password" => password}) do
     user = Repo.get_by(User, email: String.downcase(email))
-
     case check_password(user, password) do
       true -> {:ok, user}
       _ -> :error
